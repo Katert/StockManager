fba6e6dc54ea4c7baf950452d50a867b9ad7548a
Wesley Kater <wesley@codecapi.nl>
2024-04-23 11:47:21 +0200

    Fetch data on the overview page

--- app/overview/page.tsx
+++ app/overview/page.tsx
@@ -1,3 +1,15 @@
+// Types
+import type { Product } from "@/types";
+
+const getData = async (): Promise<Product[]> => {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/stock`
+  );
+  const data = await response.json();
+  return data;
+};
+
 export default async function Overview() {
+  const data: Product[] = await getData();
   return <section className="mx-16 md:mx-48"></section>;
 }
