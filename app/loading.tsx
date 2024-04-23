26e86c29db8a907c01bff8fcde4ec2106d002f2f
Wesley Kater <wesley@codecapi.nl>
2024-04-23 12:18:39 +0200

    Added a loading page for navigating between index and overview.

---
+++ app/loading.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+// Components
+import { CircleLoader } from "react-spinners";
+
+const Loading = () => {
+  return (
+    <section className="mx-48 flex flex-grow items-center justify-center">
+      <CircleLoader color="white" size={50} />
+    </section>
+  );
+};
+
+export default Loading;
