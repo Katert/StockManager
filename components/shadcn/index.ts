1d18a984b1ece6301673163d6280c0e948d3a7d0
Wesley Kater <wesley@codecapi.nl>
2024-04-23 10:30:19 +0200

    Installed required ShadCN UI components and added index.ts file

---
+++ components/shadcn/index.ts
@@ -0,0 +1,19 @@
+export { Input } from "@/components/shadcn/ui/input";
+export {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/shadcn/ui/table";
+export { Button } from "@/components/shadcn/ui/button";
+export {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+  DropdownMenuCheckboxItem,
+} from "@/components/shadcn/ui/dropdown-menu";
