f5edb5bebb4504b96d130a9b399b522415b6d401
Wesley Kater <wesley@codecapi.nl>
2024-10-02 12:52:29 +0200

    Mismatch of parameter name in function compared to function doc.

--- lib/convertCsvData.ts
+++ lib/convertCsvData.ts
@@ -7,13 +7,13 @@ import { parse } from "csv-parse/sync";
  * Converts data from a CSV file into an array.
  *
  * @param folder - The folder in which the CSV file is located.
- * @param fileName - The file name of the CSV file.
+ * @param fileName - The file name of the CSV file. Use a relative path to the file.
  * @returns An array of objects representing each row of the CSV data.
  */
-export const convertCsvData = async (relativePath: string) => {
+export const convertCsvData = async (fileName: string) => {
   try {
     // Path to the CSV file
-    const filePath = path.join(process.cwd(), relativePath);
+    const filePath = path.join(process.cwd(), fileName);
 
     // Read CSV file content
     const fileContent = await fs.readFile(filePath, "utf8");
